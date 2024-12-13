bc9200fe87bca785150975b5a4c11129c5aae3ca
Geo <ben@bendaws.net>
2024-12-13 13:38:50 -0600

    Update bot_details.py

--- bot_details.py
+++ bot_details.py
@@ -1,9 +1,13 @@
+# MechanicMonke v3.0.1
+# Optional fields are marked with an ? in the comment. Set their value to None to unselect them.
+
 # Channel Configuration
 guild_id = 0 # Enter your guild id here
 mod_update_channel = 0 # Enter the channel ID that you want mod updates in here
 
 # Mod Configuration
 mods_file = "mods.json" # Path to the mod file you want to sync with.
+online_tracking_url = None # ? READ_ONLY! Will track an online link instead of the JSON mods_file.
 
 # Bot Configuration
 bot_token = "" # Paste your bot's token here
@@ -14,4 +18,4 @@ bot_playing_url = "https://discord.com" # add a URL to your discord or whatever 
 allow_add_repositories = True # Enables / Disables the /addrepo command.
 
 # Bot Administrators
-bot_admin_ids = {} # The USER IDS of admins that can control the bot.
+bot_admin_ids = {} # ? The USER IDS of admins that can control the bot.
